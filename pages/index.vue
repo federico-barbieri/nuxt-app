e0c30ef682f106d7d55725fa5eab99d49e44b4ff
Federico Barbieri <federicobarbieri54@gmail.com>
2024-04-07 21:13:16 +0200

    bgimg

--- pages/index.vue
+++ pages/index.vue
@@ -55,33 +55,70 @@ body{
 }
 
 
-.daycares{
+.daycares {
   padding: 1rem 0;
   width: 80%;
   margin: 0 auto 0 0;
-  transition: all 0.5s ease;
-
+  position: relative; /* Add this line */
 }
 
-.daycares:hover{
-    color: #F9F7F7;
-    background-color: #35354A;
-    border-bottom: 40px solid rgb(22, 21, 21);
-    cursor: pointer;
+.daycares::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url("../public/projectsImgs/daycares.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  z-index: -1;
 }
 
-.swampfest{
+.daycares:hover::before {
+  opacity: 0.5;
+}
+
+.daycares:hover {
+  color: #f9f7f7;
+  background-color: rgba(53, 53, 74, 0.5);
+  cursor: pointer;
+}
+
+.swampfest {
   padding: 1rem 0;
-width: 80%;
-margin: 0 auto 0 0; 
-transition: all 0.5s ease;
+  width: 80%;
+  margin: 0 auto 0 0;
+  position: relative; /* Add this line */
 }
 
-.swampfest:hover{
-    color: #F9F7F7;
-    background-color: #C4D303;
-    border-bottom: 40px solid rgb(22, 21, 21);
-    cursor: pointer;
+.swampfest::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url("../public/projectsImgs/swampfest.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  z-index: -1;
+}
+
+.swampfest:hover::before {
+  opacity: 0.5;
+}
+
+.swampfest:hover {
+  color: #f9f7f7;
+  background-color: rgba(196, 211, 3, 0.5);
+  cursor: pointer;
 }
 
 .messiverse{
