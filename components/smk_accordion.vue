404309355777bb500da8352458e5c24ace7ee11f
Federico Barbieri <federicobarbieri54@gmail.com>
2025-03-22 12:05:27 +0100

    adds two components to smk

---
+++ components/smk_accordion.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const items = ref([
+  {
+    label: 'Credential Security',
+    content: 'Uses bcrypt to hash passwords and JSON Web Tokens for authentication.'
+  },
+  {
+    label: 'App Security',
+    content: 'Implements Nginx as a reverse proxy, Helmet, CORS, and Express Rate Limit among others.'
+  }
+])
+</script>
+
+<template>
+  <UAccordion :items="items" />
+</template>
---
+++ components/smk_carousel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const items = [
+  '/images/swampfest/smk/carousel/smk_1.jpg',
+  '/images/swampfest/smk/carousel/smk_2.jpg',
+  '/images/swampfest/smk/carousel/smk_3.jpg',
+  '/images/swampfest/smk/carousel/smk_4.jpg',
+  '/images/swampfest/smk/carousel/smk_5.jpg',
+  '/images/swampfest/smk/carousel/smk_6.jpg',
+  '/images/swampfest/smk/carousel/smk_7.jpg',
+  '/images/swampfest/smk/carousel/smk_8.jpg',
+  '/images/swampfest/smk/carousel/smk_9.jpg',
+
+]
+</script>
+
+<template>
+  <UCarousel v-slot="{ item }" :items="items" class="w-full mx-auto">
+    <img :src="item" width="300" height="150" class="mx-3">
+  </UCarousel>
+</template>
